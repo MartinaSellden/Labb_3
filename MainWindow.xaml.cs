774931b6386699f9b05e0ccebdfb61c58f1de8e9
Martina Selldén <martina.sellden@gmail.com>
2022-10-23 23:31:55 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labb_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public int CheckTableNumberInput()
+        {
+            string input;
+            int tableNumber;
+
+            while (tableNumberComboBox.Text=="")
+            {
+                MessageBox.Show("Du måste välja ett bordsnummer");
+            }
+            input = tableNumberComboBox.Text;
+            tableNumber = Convert.ToInt32(input);
+            return tableNumber;
+
+        }
+           public string CheckTimeInput()
+        {
+
+            string time = timeComboBox.Text.ToString();
+            while (time == null)
+            {
+                MessageBox.Show("Du måste välja en tid för att göra dn bokning");
+            }
+            
+            return time;
+           
+        }
+        public DateTime CheckDateInput()
+        {
+            DateTime input = DateTime.Now;
+            try
+            {
+                while (datepicker1.SelectedDate==null)
+                {
+                    MessageBox.Show("Du behöver välja ett datum för att slutföra bokningen");
+                }
+                input = datepicker1.SelectedDate.Value.Date;
+
+                if (input.Day<DateTime.Now.Day)
+                {
+                    MessageBox.Show("Du kan inte välja ett datum innan dagens datum");
+                    CheckDateInput();
+                }
+
+            }
+            catch(Exception e) 
+            {
+                
+            }
+            return input;
+        }
+        public string CheckNameInput()
+        {
+            string input = nameTextBox.Text;
+            string validInput;
+            try
+            {
+                while (string.IsNullOrEmpty(input))
+                {
+                    MessageBox.Show("Du behöver fylla i ett namn för bokningen, försök igen!");
+                    input = nameTextBox.Text;
+                }   
+                Regex r = new Regex(@"[a-öA-Ö]{2,}");
+
+                validInput = r.IsMatch(input) ? input: "";
+                if (string.IsNullOrEmpty(validInput))
+                {
+                    MessageBox.Show("Ogiltigt format, endast bokstäver!");
+                    CheckNameInput();
+                }
+                
+            }
+            
+            catch (Exception e)
+            {
+                //
+            }
+            return input;//[0].ToString().ToUpper() + input.Substring(1).ToLower();
+        }
+
+        private void reservationButton_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime dateInput = CheckDateInput();
+            string date = dateInput.ToShortDateString();
+
+            string name = CheckNameInput();
+
+            string time =  CheckTimeInput();
+
+            int tableNumber = CheckTableNumberInput();
+
+           
+            TableReservation.reservationList.Add(new TableReservation(name, tableNumber, date, time));
+
+            //var resservation = TableReservation.reservationList.Select(reservation => "Bokning: " + reservation.Name+reservation.Date+reservation.Time+reservation.TableNumber);
+            //string s = resservation.ToString();
+            //MessageBox.Show(s);
+
+        }
+    }
+}
---
+++ TableReservation.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace Labb_3
+{
+    internal class TableReservation
+    {
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int TableNumber { get; set; }    
+        
+
+        public static List<TableReservation> reservationList = new List<TableReservation>();
+        static List<Restaurant> restaurantsAvailable = new List<Restaurant>();
+
+        public TableReservation(string Name, int TableNumber, string Date, string Time)
+        {
+           
+            this.Name = Name;
+            this.TableNumber = TableNumber; //kolla att numret är 1-5
+            this.Date = Date; //fixa kalender man kan trycka på 
+            this.Time = Time;
+
+            //var bord = bookingList.Where(TableReservation =>TableReservation.Date == Date&&TableReservation.Time == Time)
+            //           .Select(TableReservation => TableReservation.TableNumber==TableNumber? "Bordet är bokat denna tid": TableNumber)
+            //           .ToList();
+
+        }
+
+        public void MakeReservation() //TableReservation eller void?
+        {
+
+
+            //TableReservation nyBokning = new TableReservation();
+            //bookingList.Add(nyBokning);
+            //skriv nya bokningen till fil
+        }
+
+        public void CancelReservation()
+        {
+            /*bookingList.RemoveAt(); *///hämta in index för den som ska bort
+            // ta bort bokningen från filen
+        }
+
+        public List<TableReservation> ReadFromFile(string fileName)
+        {
+           string [] lines =  File.ReadAllLines(fileName);
+           lines.ToList();
+            // få in parametrarna från listan och göra object till en lista av bokningar. 
+            //Namn som kan variera i längd bör står sist. 
+            return new List<TableReservation>();
+        }
+
+        public void DisplayReservations()
+        {
+           
+        }
+
+        //public string CheckNameInput()
+        //{
+        //    string input = ;
+        //    try
+        //    {
+        //        while (string.IsNullOrEmpty(input))
+        //        {
+        //            MessageBox.Show("Ej giltigt namn, försök igen!");
+        //            input = ;
+        //        }
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        throw;
+        //    }
+        //    return input[0].ToString().ToUpper() + input.Substring(1).ToLower();
+        //}
+
+      
+
+        //När man kollar datuminput, kolla att datumet inte är innan idag. 
+
+    }
+
+}
