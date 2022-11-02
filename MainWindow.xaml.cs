61d34bf6130f71bce3325c68910f3e6314c2b5b5
Martina Selldén <martina.sellden@gmail.com>
2022-11-02 22:36:12 +0100

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -40,102 +40,11 @@ namespace Labb_3
             UpdateReservationListBox();
         }
 
-        //private int CheckTableNumberInput()
-        //{
-        //    string input;
-        //    int tableNumber;
-
-        //    if (tableNumberComboBox.SelectedItem==null)
-        //    {
-        //        MessageBox.Show("Du måste välja ett bordsnummer", "Bordsnummer ej valt!", MessageBoxButton.OK, MessageBoxImage.Error);
-        //    }
-        //    else
-        //    {
-        //        input = tableNumberComboBox.Text;
-        //        tableNumber = Convert.ToInt32(input);
-        //        return tableNumber;
-        //    }
-
-
-        //}
-        private string CheckTimeInput()
-        {
-
-            string time = timeComboBox.Text.ToString();
-            while (timeComboBox.Text== "")
-            {
-                MessageBox.Show("Du måste välja en tid för att göra din bokning", "Tid ej vald", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            return time;
-
-        }
-        private DateTime CheckDateInput()
-        {
-            DateTime input = DateTime.Now;
-            try
-            {
-                while (datepicker1.SelectedDate==null)
-                {
-                    MessageBox.Show("Du behöver välja ett datum för att slutföra bokningen", "Datum ej valt", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-                input = datepicker1.SelectedDate.Value.Date;
-
-                if (input<DateTime.Now)
-                {
-                    MessageBox.Show("Du kan inte välja ett datum innan dagens datum", "Du kan ej göra bokningar bakåt i tiden", MessageBoxButton.OK, MessageBoxImage.Error);
-                    CheckDateInput();
-                }
-
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show(e.ToString());
-            }
-            return input;
-        }
-        private string CheckNameInput()
-        {
-            string input = nameTextBox.Text;
-            string validInput;
-            try
-            {
-                while (string.IsNullOrEmpty(input))
-                {
-                    MessageBox.Show("Du behöver fylla i ett namn för bokningen, försök igen!", "Fyll i namn för bokningen", MessageBoxButton.OK, MessageBoxImage.Error);
-                    input = nameTextBox.Text;
-                }
-                Regex r = new Regex(@"[a-öA-Ö]{2,}");
-
-                validInput = r.IsMatch(input) ? input : "";
-                if (string.IsNullOrEmpty(validInput))
-                {
-                    MessageBox.Show("Ogiltigt format, endast bokstäver!", "Ogiltigt format", MessageBoxButton.OK, MessageBoxImage.Error);
-                    CheckNameInput();
-                }
-            }
-            catch (Exception e)
-            {
-
-            }
-            return input;
-        }
-        private int CheckNumberOfGuests()
-        {
-            string numberOfGuests = GuestsComboBox.Text.ToString();
-            while (numberOfGuests == null)
-            {
-                MessageBox.Show("Du måste välja en tid för din bokning", "Tid ej vald", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            return Int32.Parse(numberOfGuests);
-        }
-
         private void UpdateReservationListBox()
         {
             reservationListBox.ItemsSource = null;
             reservationListBox.ItemsSource = TableReservation.tableReservationProperties;
         }
-
         private void Clear()
         {
             datepicker1.SelectedDate=null;
@@ -144,7 +53,6 @@ namespace Labb_3
             tableNumberComboBox.SelectedValue=null;
             GuestsComboBox.SelectedValue=null;
         }
-
         private void MakeReservation(object sender, RoutedEventArgs e)
         {
             try
@@ -168,7 +76,6 @@ namespace Labb_3
                     {
                         MessageBox.Show("Du behöver välja ett datum för att slutföra bokningen", "Datum ej valt", MessageBoxButton.OK, MessageBoxImage.Error);
                     }
-
                     if (nameTextBox.Text=="")
                     {
                         MessageBox.Show("Du behöver fylla i ett namn för bokningen, försök igen!", "Fyll i namn för bokningen", MessageBoxButton.OK, MessageBoxImage.Error);
